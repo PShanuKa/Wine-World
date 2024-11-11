59a5adce8f72afa42a1bf51bb349ee2405b2876c
Pasindu shanuka <143288490+PShanuKa@users.noreply.github.com>
2024-11-11 12:58:09 +0530

    update

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,7 +2,7 @@
 
 import { Navigate, Route, Routes } from "react-router-dom";
 import { authRoutes,  IRoute, publicRoutes } from ".";
-import Default from "../layouts/Default";
+import Default from "../layout/Default";
 
 
 const AllRoutes = () => {
